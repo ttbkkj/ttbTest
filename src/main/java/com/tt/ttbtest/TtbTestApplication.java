78a172b26b181de1412e5db914ea4544ea16ce61
TTB-NB-KKJ\TTB KKJ <kj.kim@ttb.kr>
2023-09-13 11:34:42 +0900

    ttbTest

---
+++ src/main/java/com/tt/ttbtest/TtbTestApplication.java
@@ -0,0 +1,13 @@
+package com.tt.ttbtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtbTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtbTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tt/ttbtest/TtbTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tt.ttbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtbTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
